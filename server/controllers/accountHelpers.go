6839099bad332474305411831d4a0f978649cce6
Widehoo <ars.titov@gmail.com>
2023-03-19 18:44:28 +0300

    random number fixed

--- server/controllers/accountHelpers.go
+++ server/controllers/accountHelpers.go
@@ -8,11 +8,14 @@ import (
 )
 
 func newAccount(firstName, lastName, password string) *models.Account {
+	rand.Seed(time.Now().UnixNano())
+	number := 100000 + rand.Intn(900000)
+
 	return &models.Account{
 		FirstName: firstName,
 		LastName:  lastName,
 		Password:  password,
-		Number:    int64(rand.Intn(1000000)),
+		Number:    int64(number),
 		CreatedAt: time.Now().UTC(),
 	}
 }
