b51b2b426e7824a6d2da96c2563e486d6a62aab9
Widehoo <ars.titov@gmail.com>
2023-03-30 13:36:49 +0300

    client routes added

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
+import "../App.css";
+import { IconContext } from "react-icons";
+
+function Navbar() {
+  return (
+    <>
+      <IconContext.Provider value={{ color: "undefined" }}>
+        <nav className="nav-menu">
+          <ul className="nav-menu-items">
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './pages/Home'
+import Settings from './pages/Settings'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import History from './pages/History'
+
+const AppLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "settings",
+        element: <Settings />,
+      },
+      {
+        path: "history",
+        element: <History />,
+      },
+    ],
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "signup",
+    element: <Signup />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
